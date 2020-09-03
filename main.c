c372ea4ff99d38ec19c7e44e98ca7caaff326914
YangJinfan <jqy5460@psu.edu>
2020-09-03 17:41:00 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+// Author: Jinfan Yang jqy5460@psu.edu
+// Collaborator: Thomas Huggett tfh5238@psu.edu
+// Collaborator: Wenrui Zhang wkz5094@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int main(void) {
+  char *Celsius = readline("Enter temperature in celsius: ");
+  double C = atof(Celsius);
+  double F = C*9/5+32;
+  printf("%f° in Celsius is equavalent to %f° Fahrenheit.\n",C,F);
+  return 0;
+}
